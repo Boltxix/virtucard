0f438435396e0b0b29b230852242f91b2f8e5aaa
Maciek Sobiecki <sobiecki.maciek@gmail.com>
2023-05-11 22:01:21 +0100

    Added a featur to the navbar so that when user selectes a category it highlights which category the user is browsing

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react' // Import React and useContext hook
+import React, { useContext, useState } from 'react' // Import React and useContext hook
 import Logo from "../img/logo.png" // Import logo image
 import { Link } from 'react-router-dom' // Import Link component from react-router-dom
 import { AuthContext } from '../context/authContext' // Import AuthContext from authContext.js
@@ -7,7 +7,18 @@ const Navbar = () => { // Define Navbar functional component
 
   const { currentUser, logout } = useContext(AuthContext); // Use useContext hook to access currentUser and logout functions from AuthContext
 
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
+  const handleCategoryClick = (category) => {
+    if (selectedCategory === category) {
+      setSelectedCategory(null);
+    }
+    setSelectedCategory(category);
+  }
+
+  const handleLogoClick = () => {
+    setSelectedCategory(null);
+  }
 
 
   return ( // Render the following JSX
@@ -15,38 +26,38 @@ const Navbar = () => { // Define Navbar functional component
       <div className="container">
         <div className="logo">
           {/*Render a Link component that links to the home page */}
-          <Link to="/">
+          <Link to="/" onClick={handleLogoClick}>
             <img src={Logo} alt="" />
           </Link>
         </div>
         {/* Render links that link to different categorys */}
         <div className="links">
-          <Link className='link' to="/?cat=birthday">
+          <Link className={`link ${selectedCategory === 'birthday' ? 'selected' : ''}`} to="/?cat=birthday" onClick={() => handleCategoryClick('birthday')}>
             <h6>BIRTHDAY</h6>
           </Link>
-          <Link className='link' to="/?cat=events">
+          <Link className={`link ${selectedCategory === 'events' ? 'selected' : ''}`} to="/?cat=events" onClick={() => handleCategoryClick('events')}>
             <h6>EVENTS</h6>
           </Link>
-          <Link className='link' to="/?cat=wedding">
+          <Link className={`link ${selectedCategory === 'wedding' ? 'selected' : ''}`} to="/?cat=wedding" onClick={() => handleCategoryClick('wedding')}>
             <h6>WEDDING</h6>
           </Link>
-          <Link className='link' to="/?cat=valentines">
+          <Link className={`link ${selectedCategory === 'valentines' ? 'selected' : ''}`} to="/?cat=valentines" onClick={() => handleCategoryClick('valentines')}>
             <h6>VALENTINES</h6>
           </Link>
-          <Link className='link' to="/?cat=christmas">
+          <Link className={`link ${selectedCategory === 'christmas' ? 'selected' : ''}`} to="/?cat=christmas" onClick={() => handleCategoryClick('christmas')}>
             <h6>CHRISTMAS</h6>
           </Link>
-          <Link className='link' to="/profile">
+          <Link className='link' to="/profile" onClick={handleLogoClick}>
             <span>{currentUser?.username}</span>
           </Link>
           {/* If the user is logged in, render the following JSX, Render a Link component that links to the user's events page */}
           {currentUser && (
-            <Link className='link' to="/events">
+            <Link className='link' to="/events" onClick={handleLogoClick}>
               <span>Your Events</span>
             </Link>
           )}
           {/* If the user is logged in, render a Link component that logs the user out when clicked; otherwise, render a Link component that links to the login page */}
-          {currentUser ? <Link className='link' to='/'><span onClick={logout}>Logout</span></Link> : <Link className='link' to="/login">Login</Link>}
+          {currentUser ? <Link className='link' to='/' onClick={handleLogoClick}><span onClick={logout}>Logout</span></Link> : <Link className='link' to="/login">Login</Link>}
           <span className='write'>
             <Link className='link' to="/write">Post</Link>
           </span>
